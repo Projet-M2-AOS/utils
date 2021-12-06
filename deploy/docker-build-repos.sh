32a4cdb4bc91500ef583c1d1110c06670a1d7981
b4cktr4ck5r3 <flennertom@gmail.com>
2021-12-06 23:37:29 +0100

    fix: add nocolor on the end of each echo (better readability for multi cmd)

--- deploy/docker-build-repos.sh
+++ deploy/docker-build-repos.sh
@@ -2,54 +2,54 @@ GREEN=$'\e[0;32m'
 RED=$'\e[0;31m'
 NC=$'\e[0m'
 
-echo "${GREEN}Building docker image of repos..."
+echo "${GREEN}Building docker image of repos...${NC}"
 
-echo "${GREEN}Building micro-rating..."
+echo "${GREEN}Building micro-rating...${NC}"
 docker build ./micro-rating -t micro-rating 2>&1> /dev/null
 if [ $? -eq 0 ]; then
-    echo "${GREEN}micro-rating image is available !\n"
+    echo "${GREEN}micro-rating image is available !\n${NC}"
 else
-    echo "${RED}micro-rating image is not available, build failed !\n"
+    echo "${RED}micro-rating image is not available, build failed !\n${NC}"
 fi
 
-echo "${GREEN}Building micro-order..."
+echo "${GREEN}Building micro-order...${NC}"
 docker build ./micro-order -t micro-order 2>&1> /dev/null
 if [ $? -eq 0 ]; then
-    echo "${GREEN}micro-order image is available !\n"
+    echo "${GREEN}micro-order image is available !\n${NC}"
 else
-    echo "${RED}micro-order image is not available, build failed !\n"
+    echo "${RED}micro-order image is not available, build failed !\n${NC}"
 fi
 
-echo "${GREEN}Building micro-user..."
+echo "${GREEN}Building micro-user...${NC}"
 docker build ./micro-user -t micro-user 2>&1> /dev/null
 if [ $? -eq 0 ]; then
-    echo "${GREEN}micro-user image is available !\n"
+    echo "${GREEN}micro-user image is available !\n${NC}"
 else
-    echo "${RED}micro-user image is not available, build failed !\n"
+    echo "${RED}micro-user image is not available, build failed !\n${NC}"
 fi
-echo "${GREEN}Building micro-products..."
+echo "${GREEN}Building micro-products...${NC}"
 docker build ./micro-products -t micro-products 2>&1> /dev/null
 if [ $? -eq 0 ]; then
-    echo "${GREEN}micro-products image is available !\n"
+    echo "${GREEN}micro-products image is available !\n${NC}"
 else
-    echo "${RED}micro-products image is not available, build failed !\n"
+    echo "${RED}micro-products image is not available, build failed !\n${NC}"
 fi
 
-echo "${GREEN}Building micro-product-list"
+echo "${GREEN}Building micro-product-list${NC}"
 docker build ./micro-product-lists -t micro-product-list 2>&1> /dev/null
 if [ $? -eq 0 ]; then
-    echo "${GREEN}micro-product-list image is available !\n"
+    echo "${GREEN}micro-product-list image is available !\n${NC}"
 else
-    echo "${RED}micro-product-list image is not available, build failed !\n"
+    echo "${RED}micro-product-list image is not available, build failed !\n${NC}"
 fi
 
-echo "${GREEN}Building micro-comments"
+echo "${GREEN}Building micro-comments${NC}"
 docker build ./micro-comments -t micro-comments 2>&1> /dev/null
 if [ $? -eq 0 ]; then
-    echo "${GREEN}micro-comments image is available !\n"
+    echo "${GREEN}micro-comments image is available !\n${NC}"
 else
-    echo "${RED}micro-comments image is not available, build failed !\n"
+    echo "${RED}micro-comments image is not available, build failed !\n${NC}"
 fi
 
-echo "${GREEN}Docker images builded !"
+echo "${GREEN}Docker images builded !${NC}"
 
--- deploy/get-repos.sh
+++ deploy/get-repos.sh
@@ -2,54 +2,54 @@ GREEN=$'\e[0;32m'
 RED=$'\e[0;31m'
 NC=$'\e[0m'
 
-echo "${GREEN}Downloading repos...\n"
+echo "${GREEN}Downloading repos...\n${NC}"
 
-echo "${GREEN}Downloading micro-rating..."
+echo "${GREEN}Downloading micro-rating...${NC}"
 git clone https://github.com/Projet-M2-AOS/micro-rating.git 2>&1> /dev/null
 if [ $? -eq 0 ]; then
-    echo "${GREEN}Micro-rating downloaded !\n"
+    echo "${GREEN}Micro-rating downloaded !\n${NC}"
 else
-    echo "${RED}Micro-rating download failed !\n"
+    echo "${RED}Micro-rating download failed !\n${NC}"
 fi
 
-echo "${GREEN}Downloading micro-order..."
+echo "${GREEN}Downloading micro-order...${NC}"
 git clone https://github.com/Projet-M2-AOS/micro-order.git 2>&1> /dev/null
 if [ $? -eq 0 ]; then
-    echo "${GREEN}Micro-order downloaded !\n"
+    echo "${GREEN}Micro-order downloaded !\n${NC}"
 else
-    echo "${RED}Micro-order download failed !\n"
+    echo "${RED}Micro-order download failed !\n${NC}"
 fi
 
-echo "${GREEN}Downloading micro-user..."
+echo "${GREEN}Downloading micro-user...${NC}"
 git clone https://github.com/Projet-M2-AOS/micro-user.git  2>&1> /dev/null
 if [ $? -eq 0 ]; then
-    echo "${GREEN}Micro-user downloaded !\n"
+    echo "${GREEN}Micro-user downloaded !\n${NC}"
 else
-    echo "${RED}Micro-user download failed !\n"
+    echo "${RED}Micro-user download failed !\n${NC}"
 fi
 
-echo "${GREEN}Downloading micro-products..."
+echo "${GREEN}Downloading micro-products...${NC}"
 git clone https://github.com/Projet-M2-AOS/micro-products.git 2>&1> /dev/null
 if [ $? -eq 0 ]; then
-    echo "${GREEN}Micro-products downloaded !\n"
+    echo "${GREEN}Micro-products downloaded !\n${NC}"
 else
-    echo "${RED}Micro-products download failed !\n"
+    echo "${RED}Micro-products download failed !\n${NC}"
 fi
 
-echo "Downloading micro-product-list"
+echo "Downloading micro-product-list${NC}"
 git clone https://github.com/Projet-M2-AOS/micro-product-lists.git 2>&1> /dev/null
 if [ $? -eq 0 ]; then
-    echo "${GREEN}Micro-product-list downloaded !\n"
+    echo "${GREEN}Micro-product-list downloaded !\n${NC}"
 else
-    echo "${RED}Micro-product-list download failed !\n"
+    echo "${RED}Micro-product-list download failed !\n${NC}"
 fi
 
-echo "Downloading micro-comments"
+echo "Downloading micro-comments${NC}"
 git clone https://github.com/Projet-M2-AOS/micro-comments.git 2>&1> /dev/null
 if [ $? -eq 0 ]; then
-    echo "${GREEN}Micro-comments downloaded !\n"
+    echo "${GREEN}Micro-comments downloaded !\n${NC}"
 else
-    echo "${RED}Micro-comments download failed !\n"
+    echo "${RED}Micro-comments download failed !\n${NC}"
 fi
 
-echo "${GREEN}Repos downloaded !"
+echo "${GREEN}Repos downloaded !${NC}"
