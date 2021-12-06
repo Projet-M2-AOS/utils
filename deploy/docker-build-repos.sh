5077b4b8846aefe950cad18e5d7cd221a4ea7624
b4cktr4ck5r3 <flennertom@gmail.com>
2021-12-06 23:34:23 +0100

    feat: add colors, and exit error management

--- deploy/docker-build-repos.sh
+++ deploy/docker-build-repos.sh
@@ -1,28 +1,55 @@
-echo "Building docker image of repos..."
+GREEN=$'\e[0;32m'
+RED=$'\e[0;31m'
+NC=$'\e[0m'
 
-echo "Building micro-rating..."
+echo "${GREEN}Building docker image of repos..."
+
+echo "${GREEN}Building micro-rating..."
 docker build ./micro-rating -t micro-rating 2>&1> /dev/null
-echo "micro-rating image is available !\n"
+if [ $? -eq 0 ]; then
+    echo "${GREEN}micro-rating image is available !\n"
+else
+    echo "${RED}micro-rating image is not available, build failed !\n"
+fi
 
-echo "Building micro-order..."
+echo "${GREEN}Building micro-order..."
 docker build ./micro-order -t micro-order 2>&1> /dev/null
-echo "micro-order image is available !\n"
+if [ $? -eq 0 ]; then
+    echo "${GREEN}micro-order image is available !\n"
+else
+    echo "${RED}micro-order image is not available, build failed !\n"
+fi
 
-echo "Building micro-user..."
+echo "${GREEN}Building micro-user..."
 docker build ./micro-user -t micro-user 2>&1> /dev/null
-echo "micro-user image is available !\n"
-
-echo "Building micro-products..."
+if [ $? -eq 0 ]; then
+    echo "${GREEN}micro-user image is available !\n"
+else
+    echo "${RED}micro-user image is not available, build failed !\n"
+fi
+echo "${GREEN}Building micro-products..."
 docker build ./micro-products -t micro-products 2>&1> /dev/null
-echo "micro-products image is available !\n"
+if [ $? -eq 0 ]; then
+    echo "${GREEN}micro-products image is available !\n"
+else
+    echo "${RED}micro-products image is not available, build failed !\n"
+fi
 
-echo "Building micro-prouct-list"
+echo "${GREEN}Building micro-product-list"
 docker build ./micro-product-lists -t micro-product-list 2>&1> /dev/null
-echo "micro-product-list image is available !\n"
+if [ $? -eq 0 ]; then
+    echo "${GREEN}micro-product-list image is available !\n"
+else
+    echo "${RED}micro-product-list image is not available, build failed !\n"
+fi
 
-echo "Building micro-comments"
+echo "${GREEN}Building micro-comments"
 docker build ./micro-comments -t micro-comments 2>&1> /dev/null
-echo "micro-comments image is available !\n"
+if [ $? -eq 0 ]; then
+    echo "${GREEN}micro-comments image is available !\n"
+else
+    echo "${RED}micro-comments image is not available, build failed !\n"
+fi
 
-echo "Docker images builded !"
+echo "${GREEN}Docker images builded !"
 
--- deploy/get-repos.sh
+++ deploy/get-repos.sh
@@ -1,27 +1,55 @@
-echo "Downloading repos...\n"
+GREEN=$'\e[0;32m'
+RED=$'\e[0;31m'
+NC=$'\e[0m'
 
-echo "Downloading micro-rating..."
+echo "${GREEN}Downloading repos...\n"
+
+echo "${GREEN}Downloading micro-rating..."
 git clone https://github.com/Projet-M2-AOS/micro-rating.git 2>&1> /dev/null
-echo "Micro-rating downloaded !\n"
+if [ $? -eq 0 ]; then
+    echo "${GREEN}Micro-rating downloaded !\n"
+else
+    echo "${RED}Micro-rating download failed !\n"
+fi
 
-echo "Downloading micro-order..."
+echo "${GREEN}Downloading micro-order..."
 git clone https://github.com/Projet-M2-AOS/micro-order.git 2>&1> /dev/null
-echo "Micro-order downloaded !\n"
+if [ $? -eq 0 ]; then
+    echo "${GREEN}Micro-order downloaded !\n"
+else
+    echo "${RED}Micro-order download failed !\n"
+fi
 
-echo "Downloading micro-user..."
+echo "${GREEN}Downloading micro-user..."
 git clone https://github.com/Projet-M2-AOS/micro-user.git  2>&1> /dev/null
-echo "Micro-user downloaded !\n"
+if [ $? -eq 0 ]; then
+    echo "${GREEN}Micro-user downloaded !\n"
+else
+    echo "${RED}Micro-user download failed !\n"
+fi
 
-echo "Downloading micro-products..."
+echo "${GREEN}Downloading micro-products..."
 git clone https://github.com/Projet-M2-AOS/micro-products.git 2>&1> /dev/null
-echo "Micro-products downloaded !\n"
+if [ $? -eq 0 ]; then
+    echo "${GREEN}Micro-products downloaded !\n"
+else
+    echo "${RED}Micro-products download failed !\n"
+fi
 
-echo "Downloading micro-prouct-list"
+echo "Downloading micro-product-list"
 git clone https://github.com/Projet-M2-AOS/micro-product-lists.git 2>&1> /dev/null
-echo "Micro-product-list downloaded !\n"
+if [ $? -eq 0 ]; then
+    echo "${GREEN}Micro-product-list downloaded !\n"
+else
+    echo "${RED}Micro-product-list download failed !\n"
+fi
 
 echo "Downloading micro-comments"
 git clone https://github.com/Projet-M2-AOS/micro-comments.git 2>&1> /dev/null
-echo "Micro-comments downloaded !\n"
+if [ $? -eq 0 ]; then
+    echo "${GREEN}Micro-comments downloaded !\n"
+else
+    echo "${RED}Micro-comments download failed !\n"
+fi
 
-echo "Repos downloaded !"
+echo "${GREEN}Repos downloaded !"
